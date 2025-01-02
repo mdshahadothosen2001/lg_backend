6a85d2c9410b97a884130edd93a92c218be6518c
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2025-01-02 11:55:33 +0600

    feat: add api url

--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,8 @@ from django.urls import include, path
 
 urlpatterns = [
     path("engage/admin/", admin.site.urls),
+    path('locations/api/', include('engage.locations.api.urls')),
+    path('service/api/', include('engage.service.api.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
