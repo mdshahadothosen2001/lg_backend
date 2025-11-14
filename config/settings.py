7130362a82f674d2a7bb3198512d8997d8d61e42
Shahadot Hosen <79787147+mdshahadothosen2001@users.noreply.github.com>
2025-11-14 14:26:53 +0600

    Mysql

--- config/settings.py
+++ config/settings.py
@@ -82,11 +82,19 @@ TEMPLATES = [
 WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ces',
+        'USER': 'admin',
+        'PASSWORD': '12345@admin',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
