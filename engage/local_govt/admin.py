8d99bcc3e772b74d15bc780458cb11e235ab7cf7
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2025-11-14 07:23:39 +0600

    Enhance admin interface for Member, Contribution, and Service models with custom display methods and improved field organization

--- engage/local_govt/admin.py
+++ engage/local_govt/admin.py
@@ -1,34 +1,139 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from engage.local_govt.models import Member, Contribution, VoteBooth, Vote
 
 
 
+# ----------------------------
+#       MEMBERS ADMIN CONFIG
+# ----------------------------
+
 @admin.register(Member)
 class MemberAdmin(admin.ModelAdmin):
-    list_display = ['id', 'position', 'user', 'union', 'is_verified', 'areas', 'start_at', 'end_at', 'is_active']
-    search_fields = ['position']
-    ordering = ['position']
+
+    # What to show in the list page
+    list_display = (
+        'id',
+        'position',
+        'get_user_name',       # Custom readable user
+        'union',
+        'areas',
+        'start_at',
+        'end_at',
+        'is_active_badge',     # Custom colored badge
+        'is_verified_badge',   # Custom colored badge
+    )
+
+    # Filters in sidebar
+    list_filter = (
+        'is_active',
+        'is_verified',
+        'union',
+        'start_at',
+        'end_at',
+    )
+
+    # Searchable fields
+    search_fields = (
+        'position',
+        'user__username',
+        'user__name',
+        'union__name',
+        'areas',
+    )
+
+    # Ordering
+    ordering = ('position',)
+
+    # ---------- Custom display methods ----------
+
+    def get_user_name(self, obj):
+        """Display the user's name instead of user object."""
+        return obj.user.name if obj.user else "No User"
+    get_user_name.short_description = "User"
+
+    def is_active_badge(self, obj):
+        """Colored badge for active status."""
+        if obj.is_active:
+            return format_html(
+                '<span style="color: green; font-weight: bold;">✔ Active</span>'
+            )
+        return format_html(
+            '<span style="color: red; font-weight: bold;">✖ Inactive</span>'
+        )
+    is_active_badge.short_description = "Active Status"
+    is_active_badge.admin_order_field = 'is_active'
+
+    def is_verified_badge(self, obj):
+        """Colored badge for verified status."""
+        if obj.is_verified:
+            return format_html(
+                '<span style="color: blue; font-weight: bold;">✔ Verified</span>'
+            )
+        return format_html(
+            '<span style="color: gray; font-weight: bold;">✖ Not Verified</span>'
+        )
+    is_verified_badge.short_description = "Verified"
+    is_verified_badge.admin_order_field = 'is_verified'
 
 
+# ----------------------------
+#     CONTRIBUTIONS ADMIN CONFIG
+# ----------------------------
 @admin.register(Contribution)
 class ContributionAdmin(admin.ModelAdmin):
-    list_display = ('project_title', 'start_at', 'end_at', 'is_active')
-    list_filter = ('is_active', 'start_at', 'end_at')
-    search_fields = ('project_title', 'areas')
+
+    # Show ALL important fields in list view
+    list_display = (
+        'project_title',
+        'get_contributors',        # M2M display
+        'union',
+        'areas',
+        'start_at',
+        'end_at',
+        'is_active',
+        'member',
+    )
+
+    # Filters on sidebar
+    list_filter = (
+        'is_active',
+    )
+
+    # Searchable fields
+    search_fields = (
+        'project_title',
+        'areas',
+        'union__name',     # If union has a name field
+        'member__name',    # If member has a name field
+        'Contributor__username',
+    )
+
+    # For easier M2M selection
     filter_horizontal = ('Contributor',)
 
+    # --------- Custom Display Methods ---------
 
-@admin.register(VoteBooth)
-class VoteBoothAdmin(admin.ModelAdmin):
-    list_display = ('vote_title', 'start_at', 'end_at', 'is_active')
-    search_fields = ('vote_title',)
-    list_filter = ('is_active', 'start_at', 'end_at')
-    filter_horizontal = ('candidates', 'voters')
+    def get_contributors(self, obj):
+        """
+        Shows all contributors' names as comma-separated text.
+        """
+        return ", ".join([user.username for user in obj.Contributor.all()])
+
+    get_contributors.short_description = "Contributors"
 
 
-@admin.register(Vote)
-class VoteAdmin(admin.ModelAdmin):
-    list_display = ('vote_booth', 'voter', 'vote', 'time', 'is_active')
-    search_fields = ('vote_booth__vote_title', 'voter__username')
-    list_filter = ('is_active', 'time')
+# @admin.register(VoteBooth)
+# class VoteBoothAdmin(admin.ModelAdmin):
+#     list_display = ('vote_title', 'start_at', 'end_at', 'is_active')
+#     search_fields = ('vote_title',)
+#     list_filter = ('is_active', 'start_at', 'end_at')
+#     filter_horizontal = ('candidates', 'voters')
+
+
+# @admin.register(Vote)
+# class VoteAdmin(admin.ModelAdmin):
+#     list_display = ('vote_booth', 'voter', 'vote', 'time', 'is_active')
+#     search_fields = ('vote_booth__vote_title', 'voter__username')
+#     list_filter = ('is_active', 'time')
--- engage/request/admin.py
+++ engage/request/admin.py
@@ -1,6 +1,6 @@
-# admin.py
 from django.contrib import admin
 from django.utils.html import format_html
+
 from engage.request.models import Request, RespondImage, Solution, SolutionVote
 
 
@@ -15,7 +15,7 @@ class RespondImageInline(admin.TabularInline):
 
     def image_preview(self, obj):
         if obj.image:
-            return format_html('<img src="{}" style="height: 80px;"/>', obj.image.url)
+            return format_html('<img src="{}" style="height:80px; border-radius:5px;" />', obj.image.url)
         return "No Image"
     image_preview.short_description = 'Preview'
 
@@ -26,9 +26,19 @@ class RespondImageInline(admin.TabularInline):
 class SolutionInline(admin.TabularInline):
     model = Solution
     extra = 1
-    fields = ['suggested_by', 'description', 'file', 'is_open_for_vote', 'is_best']
-    readonly_fields = []
-    show_change_link = True  # solution এ link দিবে যাতে edit page এ যাওয়া যায়
+    fields = ['suggested_by', 'short_description', 'file_link', 'is_open_for_vote', 'is_best']
+    readonly_fields = ['short_description', 'file_link']
+    show_change_link = True  # Makes each solution clickable to edit page
+
+    def short_description(self, obj):
+        return (obj.description[:50] + '...') if len(obj.description) > 50 else obj.description
+    short_description.short_description = "Description"
+
+    def file_link(self, obj):
+        if obj.file:
+            return format_html('<a href="{}" target="_blank">Open File</a>', obj.file)
+        return "-"
+    file_link.short_description = "File"
 
 
 # -------------------------
@@ -36,23 +46,35 @@ class SolutionInline(admin.TabularInline):
 # -------------------------
 @admin.register(Request)
 class RequestAdmin(admin.ModelAdmin):
+
+    list_display = [
+        'id', 'title', 'requested_citizen', 'union_display', 'taken_member',
+        'status_badge', 'possibility_amount', 'possibility_time_required',
+        'created_at', 'modified_at'
+    ]
+    list_display_links = ['id', 'title']
+    search_fields = ['title', 'description', 'requested_citizen__username', 'taken_member__user__username']
+    ordering = ['-created_at']
+    list_filter = ['status', 'union', 'taken_member']
+
+    inlines = [RespondImageInline, SolutionInline]
+
     def union_display(self, obj):
         if obj.union:
             return f"{obj.union.name} ({obj.union.id})"
         return "No Union"
     union_display.short_description = 'Union'
 
-    list_display = [
-        'id', 'title', 'requested_citizen', 'union_display', 'taken_member',
-        'status', 'possibility_amount', 'possibility_time_required',
-        'created_at', 'modified_at'
-    ]
-    list_display_links = ['id', 'title']
-    search_fields = ['title']
-    ordering = ['status']
-    list_filter = ['status']
-
-    inlines = [RespondImageInline, SolutionInline]
+    def status_badge(self, obj):
+        color = {
+            'pending': 'gray',
+            'acepted': 'blue',
+            'canceled': 'red',
+            'on_progress': 'orange',
+            'done': 'green'
+        }.get(obj.status, 'black')
+        return format_html('<span style="color:{}; font-weight:bold;">{}</span>', color, obj.status.capitalize())
+    status_badge.short_description = "Status"
 
 
 # -------------------------
@@ -61,15 +83,35 @@ class RequestAdmin(admin.ModelAdmin):
 @admin.register(Solution)
 class SolutionAdmin(admin.ModelAdmin):
     list_display = [
-        'id', 'request', 'suggested_by', 'short_description',
-        'is_open_for_vote', 'is_best', 'created_at'
+        'id', 'request_link', 'suggested_by', 'short_description',
+        'file_link', 'is_open_for_vote_badge', 'is_best_badge', 'created_at'
     ]
     list_filter = ['is_open_for_vote', 'is_best']
-    search_fields = ['description', 'suggested_by__username']
+    search_fields = ['description', 'suggested_by__username', 'request__title']
 
     def short_description(self, obj):
         return (obj.description[:50] + "...") if len(obj.description) > 50 else obj.description
     short_description.short_description = "Description"
+
+    def request_link(self, obj):
+        return format_html('<a href="/ces/request/request/{}/change/">{}</a>', obj.request.id, obj.request.title)
+    request_link.short_description = "Request"
+
+    def file_link(self, obj):
+        if obj.file:
+            return format_html('<a href="{}" target="_blank">Open File</a>', obj.file)
+        return "-"
+    file_link.short_description = "File"
+
+    def is_open_for_vote_badge(self, obj):
+        color = 'green' if obj.is_open_for_vote else 'gray'
+        return format_html('<span style="color:{};">{}</span>', color, obj.is_open_for_vote)
+    is_open_for_vote_badge.short_description = "Voting Open"
+
+    def is_best_badge(self, obj):
+        color = 'blue' if obj.is_best else 'gray'
+        return format_html('<span style="color:{};">{}</span>', color, obj.is_best)
+    is_best_badge.short_description = "Best Solution"
 
 
 # -------------------------
@@ -77,6 +119,16 @@ class SolutionAdmin(admin.ModelAdmin):
 # -------------------------
 @admin.register(SolutionVote)
 class SolutionVoteAdmin(admin.ModelAdmin):
-    list_display = ['id', 'solution', 'voted_by', 'value', 'created_at']
+    list_display = ['id', 'solution_link', 'voted_by', 'value_badge', 'created_at']
+    list_display_links = ['id', 'solution_link']
     list_filter = ['value']
     search_fields = ['solution__description', 'voted_by__username']
+
+    def solution_link(self, obj):
+        return format_html('<a href="/ces/request/solution/{}/change/">{}</a>', obj.solution.id, obj.solution)
+    solution_link.short_description = "Solution"
+
+    def value_badge(self, obj):
+        color = 'green' if obj.value else 'red'
+        return format_html('<span style="color:{}; font-weight:bold;">{}</span>', color, 'Upvote' if obj.value else 'Downvote')
+    value_badge.short_description = "Vote"
