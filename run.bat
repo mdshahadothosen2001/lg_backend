d67a915a1732c4a43e9307de8e503d0ac1c3239c
Shahadot Hosen <79787147+mdshahadothosen2001@users.noreply.github.com>
2025-11-14 15:28:09 +0600

    Add fixture loading commands
    Added commands to load multiple fixture data for accounts, locations, local government, and requests.

--- run.bat
+++ run.bat
@@ -12,5 +12,17 @@ python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
 python manage.py migrate
 python manage.py collectstatic --noinput
+python manage.py loaddata engage/accounts/fixtures/m-user.json
+python manage.py loaddata engage/accounts/fixtures/g-user.json
+
+python manage.py loaddata engage/locations/fixtures/division.json
+python manage.py loaddata engage/locations/fixtures/district.json
+python manage.py loaddata engage/locations/fixtures/upazila.json
+python manage.py loaddata engage/locations/fixtures/union.json
+
+python manage.py loaddata engage/local_govt/fixtures/member.json
+python manage.py loaddata engage/local_govt/fixtures/contribution.json
+
+python manage.py loaddata engage/request/fixtures/respond.json
 echo Starting Django development server on 0.0.0.0:8000
 python manage.py runserver 0.0.0.0:8000
