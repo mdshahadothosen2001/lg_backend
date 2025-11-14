c4b9ee81259467b335fd3d5763a256ea48ccd316
Shahadot Hosen <79787147+mdshahadothosen2001@users.noreply.github.com>
2025-11-14 15:33:50 +0600

    Update run.bat to include makemigrations and service
    Added commands to make migrations and load service fixtures.

--- run.bat
+++ run.bat
@@ -10,6 +10,7 @@ IF NOT EXIST "%VENV_DIR%\Scripts\activate" (
 call %VENV_DIR%\Scripts\activate
 python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
+python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
 python manage.py loaddata engage/accounts/fixtures/m-user.json
@@ -24,5 +25,6 @@ python manage.py loaddata engage/local_govt/fixtures/member.json
 python manage.py loaddata engage/local_govt/fixtures/contribution.json
 
 python manage.py loaddata engage/request/fixtures/respond.json
+python manage.py loaddata engage/service/fixtures/service.json
 echo Starting Django development server on 0.0.0.0:8000
 python manage.py runserver 0.0.0.0:8000
